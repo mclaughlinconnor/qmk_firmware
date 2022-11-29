ad1c38b1978e4b4cf8111470116014fd4b799b3c
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-11-29 22:39:38 +0000

    Add full layers including steno

--- keyboards/mclaughlinconnor/baygullboard/keymaps/default/keymap.c
+++ keyboards/mclaughlinconnor/baygullboard/keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "keymap_steno.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
@@ -16,10 +17,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [0] = LAYOUT_split_3x6_8(
-        KC_1,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_1,
-        KC_2,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_2,
-        KC_3,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_3,
-                          KC_4,    KC_5,    KC_6,    KC_7,         KC_4,    KC_5,    KC_6,    KC_7,
-                                            KC_8,    KC_9,         KC_8,    KC_9
-    )
+        KC_TAB,      KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,              KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,        KC_BSPC,
+        KC_ESC,      KC_A,        KC_S,        KC_D,        KC_F,        KC_G,              KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     KC_QUOT,
+        DF(3),       KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,              KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,     KC_NUHS,
+                                  MO(1),       KC_LSFT,     KC_SPC,      KC_LCTL,           KC_RCTL,     MO(2),       KC_RSFT,     KC_NO,
+                                                            KC_NO,       KC_LALT,           KC_PENT,     KC_BSPC
+        ),
+    [1] = LAYOUT_split_3x6_8(
+        KC_NO,       S(KC_1),     S(KC_2),     S(KC_3),     S(KC_4),     S(KC_5),           S(KC_6),     S(KC_7),     S(KC_8),     S(KC_9),     S(KC_0),     KC_NO,
+        KC_NO,       KC_1,        KC_2,        KC_3,        KC_4,        KC_5,              KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_NO,
+        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,             KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,
+                                  KC_NO,       KC_NO,       KC_NO,       KC_NO,             KC_NO,       KC_NO,       KC_NO,       KC_NO,
+                                                            KC_NO,       KC_NO,             KC_NO,       KC_NO
+    ),
+    [2] = LAYOUT_split_3x6_8(
+        KC_NO,       KC_LBRC,     KC_RBRC,     S(KC_LBRC),  S(KC_RBRC),  KC_NO,             KC_NO,       S(KC_LBRC),  S(KC_RBRC),  KC_LBRC,     KC_RBRC,     KC_NO,
+        KC_NO,       S(KC_SCLN),  KC_MINS,     S(KC_EQL),   KC_EQL,      KC_NO,             KC_NO,       KC_PENT,     S(KC_MINS),  KC_BSPC,     KC_NO,       KC_NO,
+        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,             KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,
+                                  KC_NO,       KC_NO,       KC_NO,       KC_NO,             KC_NO,       KC_NO,       KC_NO,       KC_NO,
+                                                            KC_NO,       KC_NO,             KC_NO,       KC_NO
+    ),
+    [3] = LAYOUT_split_3x6_8(
+        STN_N1,      STN_N2,      STN_N3,      STN_N4,      STN_N5,      STN_N6,            STN_N7,      STN_N8,      STN_N9,      STN_NA,      STN_NB,      STN_NC,
+        KC_NO,       STN_S1,      STN_TL,      STN_PL,      STN_HL,      STN_ST1,           STN_ST3,     STN_FR,      STN_PR,      STN_LR,      STN_TR,      STN_DR,
+        DF(0),       STN_S2,      STN_KL,      STN_WL,      STN_RL,      STN_ST2,           STN_ST4,     STN_RR,      STN_BR,      STN_GR,      STN_SR,      STN_ZR,
+                                  KC_NO,       STN_A,       STN_O,       KC_NO,             KC_NO,       STN_E,       STN_U,       KC_NO,
+                                                            KC_NO,       KC_NO,             KC_NO,       KC_NO
+  )
 };
