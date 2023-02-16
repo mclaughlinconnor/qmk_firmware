dce2a80e946695f9cde9808f299d7b8d810b791f
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-02-16 22:15:55 +0000

    Fix spacing in keymap

--- keyboards/mclaughlinconnor/baygullboard/keymaps/default/keymap.c
+++ keyboards/mclaughlinconnor/baygullboard/keymaps/default/keymap.c
@@ -21,12 +21,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,      KC_A,        KC_S,        KC_D,        KC_F,        KC_G,              KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     KC_QUOT,
         KC_NUBS,     KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,              KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,     KC_NUHS,
                                   MO(1),       KC_LSFT,     KC_SPC,      KC_LCTL,           KC_RCTL,     KC_BSPC,     KC_RSFT,     MO(2),
-                                                            KC_LGUI,     KC_LALT,           KC_ENT,     KC_BSPC
+                                                            KC_LGUI,     KC_LALT,           KC_ENT,      KC_BSPC
         ),
     [1] = LAYOUT_split_3x6_8(
         KC_NO,       S(KC_1),     S(KC_2),     S(KC_3),     S(KC_4),     S(KC_5),           S(KC_6),     S(KC_7),     S(KC_8),     S(KC_9),     S(KC_0),     KC_NO,
         KC_NO,       KC_1,        KC_2,        KC_3,        KC_4,        KC_5,              KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_NO,
-        KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,             KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12
+        KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,             KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,
                                   KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,           KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_NO,
                                                             KC_TRNS,     KC_TRNS,           KC_TRNS,     KC_TRNS
     ),
