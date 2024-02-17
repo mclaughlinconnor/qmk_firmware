7f4a96e2751672c1fcf38f4cf9348db7001814e5
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-02-17 19:45:26 +0000

    Tidy up keymap slightly

--- keyboards/mclaughlinconnor/baygullboard/keymaps/default/keymap.c
+++ keyboards/mclaughlinconnor/baygullboard/keymaps/default/keymap.c
@@ -10,30 +10,42 @@ enum layers {
     _QWERTY = 0,
     _NUM,
     _SYM,
+    _NAV,
     _STENO,
 };
+
+#define SPC_NAV LT(_NAV, KC_SPC)
+#define BSPC_SYM LT(_SYM, KC_BSPC)
+#define SYSREQ LALT(KC_PSCR)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x6_8(
         KC_TAB,      KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,              KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,        KC_BSPC,
         KC_ESC,      KC_A,        KC_S,        KC_D,        KC_F,        KC_G,              KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     KC_QUOT,
         KC_NUBS,     KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,              KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,     KC_NUHS,
-                                  MO(1),       KC_LSFT,     KC_SPC,      KC_LCTL,           KC_RCTL,     KC_BSPC,     KC_RSFT,     MO(2),
-                                                            KC_LGUI,     KC_LALT,           KC_ENT,      KC_BSPC
+                                  MO(1),       KC_LSFT,     SPC_NAV,     KC_LCTL,           KC_RCTL,     BSPC_SYM,    KC_RSFT,     MO(2),
+                                                            KC_LGUI,     KC_LALT,           KC_ENT,      KC_PSCR
         ),
     [_NUM] = LAYOUT_split_3x6_8(
-        KC_NO,       S(KC_1),     S(KC_2),     S(KC_3),     S(KC_4),     S(KC_5),           S(KC_6),     S(KC_7),     S(KC_8),     S(KC_9),     S(KC_0),     KC_NO,
-        KC_NO,       KC_1,        KC_2,        KC_3,        KC_4,        KC_5,              KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_NO,
-        KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,             KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,
-                                  KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,           KC_TRNS,     KC_TRNS,     VIM_TOG,     KC_NO,
-                                                            KC_TRNS,     KC_TRNS,           KC_TRNS,     KC_TRNS
+        KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,            KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        XXXXXXX,
+        KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,             KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        XXXXXXX,
+        XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
+                                  _______,     _______,     _______,     _______,           _______,     _______,     VIM_TOG,     _______,
+                                                            _______,     _______,           _______,     _______
     ),
     [_SYM] = LAYOUT_split_3x6_8(
-        KC_GRAVE,    KC_LBRC,     KC_RBRC,     S(KC_LBRC),  S(KC_RBRC),  KC_TRNS,           KC_NO,       S(KC_LBRC),  S(KC_RBRC),  KC_LBRC,     KC_RBRC,     KC_NO,
-        KC_NO,       KC_TRNS,     KC_MINS,     S(KC_EQL),   KC_EQL,      KC_TRNS,           KC_LEFT,     KC_DOWN,     KC_UP,       KC_RIGHT,    KC_TRNS,     QK_REBOOT,
-        DF(3),       KC_TRNS,     KC_TRNS,     KC_TRNS,     S(KC_MINS),  KC_TRNS,           KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     CG_TOGG,
-                                  KC_TRNS,     KC_LCTL,     KC_TRNS,     KC_TRNS,           KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_NO,
-                                                            KC_TRNS,     KC_CAPS,           KC_TRNS,     KC_TRNS
+        KC_GRAVE,    KC_LBRC,     KC_RBRC,     S(KC_LBRC),  S(KC_RBRC),  XXXXXXX,           S(KC_6),     S(KC_7),     S(KC_8),     S(KC_9),     S(KC_0),     XXXXXXX,
+        KC_NO,       XXXXXXX,     KC_MINS,     S(KC_EQL),   KC_EQL,      XXXXXXX,           S(KC_1),     S(KC_2),     S(KC_3),     S(KC_4),     S(KC_5),     XXXXXXX,
+        DF(3),       XXXXXXX,     XXXXXXX,     XXXXXXX,     S(KC_MINS),  XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     CG_TOGG,
+                                  _______,     KC_LCTL,     _______,     _______,           _______,     _______,     _______,     _______,
+                                                            _______,     KC_CAPS,           _______,     _______
+    ),
+    [_NAV] = LAYOUT_split_3x6_8(
+        XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
+        XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,           KC_LEFT,     KC_DOWN,     KC_UP,       KC_RIGHT,    XXXXXXX,     XXXXXXX,
+        XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
+                                  _______,     _______,     _______,     _______,           _______,     _______,     _______,     KC_NO,
+                                                            _______,     _______,           _______,     _______
     ),
     [_STENO] = LAYOUT_split_3x6_8(
         STN_N1,      STN_N2,      STN_N3,      STN_N4,      STN_N5,      STN_N6,            STN_N7,      STN_N8,      STN_N9,      STN_NA,      STN_NB,      STN_NC,
@@ -47,7 +59,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Add double tap keys to go to weird layers like Vim or F keys
 // Could also add layer switches to number layer so two keys are required to be pressed in a certain order
 //   Use escape to go back again
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // Process case modes
